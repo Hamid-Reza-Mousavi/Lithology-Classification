53791ce3cc7704c947e61a5b603acb48f1138e4d
Esfahani98 <97799794+Esfahani98@users.noreply.github.com>
2022-08-10 21:30:58 +0430

    Update augmentation.py

--- Functions/augmentation.py
+++ Functions/augmentation.py
@@ -54,7 +54,7 @@ def data_augmentation(traindata, testdata, hiddendata):
                              tree_method='gpu_hist',
                              learning_rate=0.05), 
                 
-                #CatBoostRegressor(task_type='GPU'),
+                CatBoostRegressor(task_type='GPU'),
                 
                 #LGBMRegressor(device='gpu',
                 #            gpu_platform_id=1,
@@ -62,7 +62,7 @@ def data_augmentation(traindata, testdata, hiddendata):
                 ]
                 
   estimator_names = ['EXTREME BOOST REGRESSOR',
-                     #'CATBOOST REGRESSOR',
+                     'CATBOOST REGRESSOR',
                      #'LIGHTBOOST REGRESSOR'
                     ] # regressors names
 
