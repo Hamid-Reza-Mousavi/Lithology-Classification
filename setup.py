87b0f45f571a99137c0315acf88e23a82d703022
Hamid Reza Mousavi <97799794+Esfahani98@users.noreply.github.com>
2022-11-18 18:48:22 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
         name="Lithofacies_Classification",
-        version="0.0.1",
+        version="0.0.2",
         url="https://github.com/Esfahani98/Lithofacies_Classification.git",
         author="Hamid Reza Mousavi",
         license="IUST",
